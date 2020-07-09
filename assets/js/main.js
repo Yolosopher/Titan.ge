447b1ff09f69313e59e1319ecae09fb6c408614e
Yolosopher <61516311+Yolosopher@users.noreply.github.com>
2020-07-09 19:55:50 +0400

    a

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+// var swiper3 = new Swiper('.top-prod-sliderr', {
+//   slidesPerView: 'auto',
+//   direction: 'horizontal',
+//   navigation: {
+//       nextEl: '.top-prod-slider-next',
+//       prevEl: '.top-prod-slider-prev',
+//   }
+// });
