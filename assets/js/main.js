cedc9c17c6db8d16f698b6023bde287423475923
Yolosopher <61516311+Yolosopher@users.noreply.github.com>
2020-07-10 05:01:00 +0400

    homepage-desktop

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,18 @@
-// var swiper3 = new Swiper('.top-prod-sliderr', {
-//   slidesPerView: 'auto',
-//   direction: 'horizontal',
-//   navigation: {
-//       nextEl: '.top-prod-slider-next',
-//       prevEl: '.top-prod-slider-prev',
-//   }
-// });
+var topProductsSlider = new Swiper('.top-prod-slider', {
+  slidesPerView: 'auto',
+  direction: 'horizontal',
+  navigation: {
+      nextEl: '.top-prod-slider-next',
+      prevEl: '.top-prod-slider-prev',
+  }
+});
+$(document).ready(function () {
+    $('.slider-arrows div').on("mouseenter", function () {
+        let a = $(this).find('div').find('path');
+        a.addClass('b');
+        console.log(a);
+        // a.fadeOut(400, function () {
+        //     $(this).attr('src', src).fadeIn(400)
+        //   });
+    });
+});
