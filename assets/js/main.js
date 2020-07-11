c29f8abebf68eac0904e09f26e79b9be320be8ed
Yolosopher <61516311+Yolosopher@users.noreply.github.com>
2020-07-11 19:15:22 +0400

    add-btn click animations

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,17 +2,71 @@ var topProductsSlider = new Swiper('.top-prod-slider', {
   slidesPerView: 'auto',
   direction: 'horizontal',
   navigation: {
-      nextEl: '.top-prod-slider-next',
-      prevEl: '.top-prod-slider-prev',
-  }
+    nextEl: '.top-prod-slider-next',
+    prevEl: '.top-prod-slider-prev',
+  },
 });
-$(document).ready(function () {
-    $('.slider-arrows div').on("mouseenter", function () {
-        let a = $(this).find('div').find('path');
-        a.addClass('b');
-        console.log(a);
-        // a.fadeOut(400, function () {
-        //     $(this).attr('src', src).fadeIn(400)
-        //   });
+
+document.addEventListener('DOMContentLoaded', () => {
+  let btns = document.querySelectorAll('.top-prod-add-btn');
+
+  for (let btn of btns) {
+    // console.log(btn);
+    btn.addEventListener('click', (e) => {
+      e.preventDefault();
+      let shopCart = '.svg-shopping-cart';
+      let checkCircle = '.svg-check-circle';
+
+      if (btn.querySelector('div').innerText === 'დამატება') {
+        btn.classList.toggle('pressed');
+        btn.querySelector('div').style.display = 'none';
+        setTimeout(() => {
+          btn.querySelector(shopCart).classList.toggle('hidden');
+        }, 1000);
+        setTimeout(() => {
+          btn.querySelector(checkCircle).classList.toggle('hidden');
+        }, 1500);
+        setTimeout(() => {
+          if (btn.querySelector('div').innerText === 'დამატება') {
+            btn.querySelector('div').innerHTML = 'დამატებულია';
+            btn.querySelector('div').style.display = 'unset';
+          } else {
+            btn.querySelector('div').innerHTML = 'დამატება';
+            btn.querySelector('div').style.display = 'unset';
+          }
+        }, 3000);
+        setTimeout(() => {
+          btn.classList.remove('pressed');
+          btn.classList.toggle('pressed-out');
+        }, 2000);
+      } else { // ELSE
+        btn.classList.toggle('pressed');
+        btn.querySelector('div').style.display = 'none';
+        setTimeout(() => {
+          btn.querySelector(checkCircle).classList.toggle('hidden');
+        }, 1000);
+        setTimeout(() => {
+          btn.querySelector(shopCart).classList.toggle('hidden');
+        }, 1500);
+        setTimeout(() => {
+          if (btn.querySelector('div').innerText === 'დამატება') {
+            btn.querySelector('div').innerHTML = 'დამატებულია';
+            btn.querySelector('div').style.display = 'unset';
+          } else {
+            btn.querySelector('div').innerHTML = 'დამატება';
+            btn.querySelector('div').style.display = 'unset';
+          }
+        }, 3000);
+        setTimeout(() => {
+          btn.classList.remove('pressed');
+          btn.classList.toggle('pressed-out');
+        }, 2000);
+      }
+    //   OUT OF IF-ELSE
+      setTimeout(() => {
+        btn.classList.remove('pressed');
+        btn.classList.remove('pressed-out');
+      }, 3001);
     });
+  }
 });
