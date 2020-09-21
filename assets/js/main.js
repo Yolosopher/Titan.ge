45fcd48e17b2e82fd67013a62057ecb65e1c8ea4
Nika Nishnianidze <61516311+Yolosopher@users.noreply.github.com>
2020-09-21 12:24:02 +0400

    a

--- assets/js/main.js
+++ assets/js/main.js
@@ -346,32 +346,53 @@ function authToggle(clWidth) {
     });
     controlHeight();
 }
-// respo.click() triggers auth-toggle
+
+function goToProfileFromDesktop() {
+    $('.login').attr('href', '../pages/profile.html');
+    setTimeout(() => {
+        $('.login').trigger('click');
+    }, 0.1);
+}
+function goToProfileFromRespo() {
+    $('.respo-login').attr('href', '../pages/profile.html');
+    setTimeout(() => {
+        $('.respo-login').trigger('click');
+    }, 0.1);
+}
+// login.click()
 $(".login").click(function () {
-    clientHeight = window.innerHeight;
-    clientWidth = window.innerWidth;
-    authToggle(clientWidth);
-    if ($(".header-black-screen").hasClass("on")) {
-        $(".home-header").toggleClass("on");
-        $(".header-black-screen").toggleClass("on");
-        $("html").toggleClass("overFlow");
-        $(".header-nav").toggleClass("on");
+    if($('.login').hasClass('active')){
+        goToProfileFromDesktop();
+    } else {
+        clientHeight = window.innerHeight;
         clientWidth = window.innerWidth;
-        if (clientWidth > 1000) {
-            $("#burger-header").slideToggle(700, function () {
-                if ($(this).is(":visible")) {
-                    $(this).css("display", "flex");
-                }
-            });
+        authToggle(clientWidth);
+        if ($(".header-black-screen").hasClass("on")) {
+            $(".home-header").toggleClass("on");
+            $(".header-black-screen").toggleClass("on");
+            $("html").toggleClass("overFlow");
+            $(".header-nav").toggleClass("on");
+            clientWidth = window.innerWidth;
+            if (clientWidth > 1000) {
+                $("#burger-header").slideToggle(700, function () {
+                    if ($(this).is(":visible")) {
+                        $(this).css("display", "flex");
+                    }
+                });
+            }
         }
     }
 });
-
+// respo-login.click()
 $(".respo-login").click(function () {
-    $(this).addClass("active");
-    clientHeight = window.innerHeight;
-    clientWidth = window.innerWidth;
-    authToggle(clientWidth);
+    if($('.login').hasClass('active')){
+        goToProfileFromRespo();
+    } else {
+        $(this).addClass("active");
+        clientHeight = window.innerHeight;
+        clientWidth = window.innerWidth;
+        authToggle(clientWidth);
+    }
 });
 
 // exit auth-toggle
